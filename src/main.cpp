c6c06b6693759454acf0ec200f54fc4bbb885182
milesfried <mafriedman@smu.edu>
2021-11-13 18:01:13 -0600

    drop this please

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@
 #include <boost/graph/graphml.hpp>
 #include <boost/graph/breadth_first_search.hpp>
 #include <fstream>
-
 //struct to store the node properties of the football graph
 struct footballData{
     long node_id;
@@ -21,45 +20,6 @@ using footballGraph =  boost::adjacency_list<boost::vecS,
         boost::vecS, boost::undirectedS, footballData>;
 //shortcut for vertex_descriptor types
 using Vertex = boost::graph_traits<footballGraph>::vertex_descriptor;
-using Edge = boost::graph_traits<footballGraph>::edge_descriptor;
-template < typename Graph, typename IncomingMap, typename DistanceMap, typename PathCountMap >
-struct visitor_type : public boost::bfs_visitor<> {
-
-
-    visitor_type(IncomingMap incoming, DistanceMap distance,
-                 PathCountMap path_count,
-                 std::stack< Vertex >& ordered_vertices)
-            : incoming(incoming), distance(distance), path_count(path_count), ordered_vertices(ordered_vertices)
-            {}
-            void examine_vertex(Vertex v, Graph&){
-        ordered_vertices.push(v);
-    }
-    void tree_edge(Edge e, Graph& g){
-        Vertex v = source(e, g);
-        Vertex w = target(e, g);
-        put(distance, w, get(distance, v) + 1);
-
-        put(path_count, w, get(path_count, v));
-        incoming[w].push_back(e);
-    }
-    void non_tree_edge(Edge e, Graph& g)
-    {
-        Vertex v = source(e, g);
-        Vertex w = target(e, g);
-        if (get(distance, w) == get(distance, v) + 1)
-        {
-            put(path_count, w,
-                get(path_count, w) + get(path_count, v));
-            incoming[w].push_back(e);
-        }
-    }
-private:
-    IncomingMap incoming;
-    DistanceMap distance;
-    PathCountMap path_count;
-    std::stack< Vertex > ordered_vertices;
-    };
-
 footballGraph readMap(std::string path);
 std::vector<std::vector<Vertex>> processParentMaps(footballGraph);
 void printParentMaps(footballGraph graph, std::vector<std::vector<Vertex>> shortestPathToFrom);
@@ -83,7 +43,7 @@ int main(int argc, char* argv[])
     std::vector<std::vector<Vertex>> shortestPathToFrom;
     //runs DFS from each node to generate parent maps
     shortestPathToFrom = processParentMaps(graph);
-    //printParentMaps(graph, shortestPathToFrom);
+    printParentMaps(graph, shortestPathToFrom);
 
 }
 /**
@@ -98,14 +58,6 @@ footballGraph readMap(std::string path){
     dp.property("value", boost::get(&footballData::value, g));
     dp.property("label", boost::get(&footballData::label, g));
     boost::read_graphml(inFile, g, dp);
-    std::fstream outFile;
-    std::string path2 = "../football/football_example.dot";
-    outFile.open(path2.c_str(), std::fstream::out);
-    if(outFile.is_open()) {
-        std::cout << "file opened " << std::endl;
-        boost::write_graphviz_dp(outFile, g, dp, "node_id");
-    }
-
     return g;
 }
 
@@ -127,19 +79,14 @@ std::vector<std::vector<Vertex>> processParentMaps(footballGraph g){
         std::vector<Vertex> p(boost::num_vertices(g));
         Vertex s = vertex_idMap[*i];
         p[s] = s;
-        //call to breadth first search
-        boost::breadth_first_search(g, s,boost::visitor
-        //creates a bfs visitor to record predecessor
-        //TO ADD: PATHCOUNTER
-        (boost::make_bfs_visitor
-        (boost::record_predecessors
-        (&p[0],boost::on_tree_edge())
-          )));
+        boost::breadth_first_search(g, s, boost::visitor(boost::make_bfs_visitor
+        (boost::record_predecessors(&p[0],
+                                    boost::on_tree_edge()))));
         parentLists.push_back(p);
     }
     //printParentMaps(g, parentLists);
     //print out read in graph
-    //boost::print_graph(g);
+    boost::print_graph(g);
     return parentLists;
 }
 //takes in a graph and it's vector of predecessor maps and prints them out
@@ -152,36 +99,3 @@ void printParentMaps(footballGraph graph, std::vector<std::vector<Vertex>> short
                       print_parent<Piter>(&shortestPathToFrom[i][0]));
     }
 }
-
-//        void construct_visitor(){
-//            std::vector<footballGraph::vertices_size_type> test;
-//            boost::iterator_property_map<
-//                    std::vector<std::vector<Edge>>*
-//                    ,
-//                    boost::LvaluePropertyMapConcept<
-//                            footballData
-//                            ,
-//                            std::vector<Edge>
-//                    >
-//            > incomingMap;
-//            boost::iterator_property_map<std::vector<footballGraph::vertices_size_type>*, Vertex> distanceMap;
-//            boost::iterator_property_map<std::vector<footballGraph::degree_size_type>*, Vertex> pathCountMap;
-//
-//
-////            boost::make_iterator_property_map(test.begin(), Vertex)
-//            visitor_type
-//                    <footballGraph, incomingMap, distanceMap, pathCountMap:
-//                    visit;
-//}
-
-
-
-
-
-
-
-
-
-
-
-
