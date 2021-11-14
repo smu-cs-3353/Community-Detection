663900e72df94e546ce00977c1acbb1ff6864b70
milesfried <mafriedman@smu.edu>
2021-11-13 20:06:36 -0600

    "alleged newman algorithm"

--- src/main.cpp
+++ src/main.cpp
@@ -22,47 +22,11 @@ using footballGraph =  boost::adjacency_list<boost::vecS,
 //shortcut for vertex_descriptor types
 using Vertex = boost::graph_traits<footballGraph>::vertex_descriptor;
 using Edge = boost::graph_traits<footballGraph>::edge_descriptor;
-template < typename Graph, typename IncomingMap, typename DistanceMap, typename PathCountMap >
-struct visitor_type : public boost::bfs_visitor<> {
-
-
-    visitor_type(IncomingMap incoming, DistanceMap distance,
-                 PathCountMap path_count,
-                 std::stack< Vertex >& ordered_vertices)
-            : incoming(incoming), distance(distance), path_count(path_count), ordered_vertices(ordered_vertices)
-            {}
-            void examine_vertex(Vertex v, Graph&){
-        ordered_vertices.push(v);
-    }
-    void tree_edge(Edge e, Graph& g){
-        Vertex v = source(e, g);
-        Vertex w = target(e, g);
-        put(distance, w, get(distance, v) + 1);
-
-        put(path_count, w, get(path_count, v));
-        incoming[w].push_back(e);
-    }
-    void non_tree_edge(Edge e, Graph& g)
-    {
-        Vertex v = source(e, g);
-        Vertex w = target(e, g);
-        if (get(distance, w) == get(distance, v) + 1)
-        {
-            put(path_count, w,
-                get(path_count, w) + get(path_count, v));
-            incoming[w].push_back(e);
-        }
-    }
-private:
-    IncomingMap incoming;
-    DistanceMap distance;
-    PathCountMap path_count;
-    std::stack< Vertex > ordered_vertices;
-    };
 
 footballGraph readMap(std::string path);
 std::vector<std::vector<Vertex>> processParentMaps(footballGraph);
-void printParentMaps(footballGraph graph, std::vector<std::vector<Vertex>> shortestPathToFrom);
+void printParentMaps(footballGraph graph, std::vector<std::vector<Vertex>>& shortestPathFromTo);
+std::vector<Edge> calculateBetween(footballGraph, std::vector<std::vector<Vertex>>& shortestPathFromTo);
 //struct from boost examples used to print out the parent
 //implemented here: https://www.boost.org/doc/libs/1_77_0/libs/graph/example/bfs.cpp
 template < class ParentDecorator > struct print_parent
@@ -80,10 +44,9 @@ int main(int argc, char* argv[])
 {   //creates a graph object of the graphml file provided
     footballGraph graph = readMap(argv[1]);
     //initializes a vector to store parent maps
-    std::vector<std::vector<Vertex>> shortestPathToFrom;
+    std::vector<std::vector<Vertex>> shortestPathFromTo;
     //runs DFS from each node to generate parent maps
-    shortestPathToFrom = processParentMaps(graph);
-    //printParentMaps(graph, shortestPathToFrom);
+    shortestPathFromTo = processParentMaps(graph);
 
 }
 /**
@@ -102,7 +65,7 @@ footballGraph readMap(std::string path){
     std::string path2 = "../football/football_starting_example.dot";
     outFile.open(path2.c_str(), std::fstream::out);
     if(outFile.is_open()) {
-        std::cout << "file opened " << std::endl;
+        //std::cout << "file opened " << std::endl;
         boost::write_graphviz_dp(outFile, g, dp, "node_id");
     }
 
@@ -143,36 +106,84 @@ std::vector<std::vector<Vertex>> processParentMaps(footballGraph g){
     return parentLists;
 }
 //takes in a graph and it's vector of predecessor maps and prints them out
+typedef std::vector<Vertex>::value_type *Piter;
 //implemented here: https://www.boost.org/doc/libs/1_77_0/libs/graph/example/bfs.cpp
-void printParentMaps(footballGraph graph, std::vector<std::vector<Vertex>> shortestPathToFrom){
-    for(int i = 0; i < shortestPathToFrom.size(); i++){
-        typedef std::vector<Vertex>::value_type *Piter;
-        std::cout << "=========================================" <<  std::endl << "Printing Predecessor Map For Node " << i << std::endl;
+void printParentMaps(footballGraph graph, std::vector<std::vector<Vertex>>& shortestPathFromTo){
+    for(int i = 0; i < shortestPathFromTo.size(); i++){
+        std::cout << "=========================================" <<  std::endl << "Printing Predecessor Map For Node "<< i <<  std::endl;
         std::for_each(boost::vertices(graph).first, boost::vertices(graph).second,
-                      print_parent<Piter>(&shortestPathToFrom[i][0]));
+                      print_parent<Piter>(&shortestPathFromTo[1][0]));
     }
 }
+using EdgeIterator = boost::graph_traits<footballGraph>::edge_iterator;
 
-//        void construct_visitor(){
-//            std::vector<footballGraph::vertices_size_type> test;
-//            boost::iterator_property_map<
-//                    std::vector<std::vector<Edge>>*
-//                    ,
-//                    boost::LvaluePropertyMapConcept<
-//                            footballData
-//                            ,
-//                            std::vector<Edge>
-//                    >
-//            > incomingMap;
-//            boost::iterator_property_map<std::vector<footballGraph::vertices_size_type>*, Vertex> distanceMap;
-//            boost::iterator_property_map<std::vector<footballGraph::degree_size_type>*, Vertex> pathCountMap;
+std::vector<Edge> calculateBetween(footballGraph graph, std::vector<std::vector<Vertex>>& shortestPathFromTo){
+    footballGraph::edge_iterator edge, edge_end;
+    std::pair<EdgeIterator, EdgeIterator> ei = boost::edges(graph);
+    std::vector<Edge> maxEdges;
+    //for each edge
+    int betweenValMax = -1;
+    for (EdgeIterator edge_iter = ei.first; edge_iter != ei.second; ++edge_iter) {
+        int betweenVal = 0;
+        Edge toCheck = *edge_iter;
+        //for each shortest path
+        for(int i = 0; i < shortestPathFromTo.size(); i++){
+            for(int j =0; j < shortestPathFromTo[i].size(); j++){
+               bool check = true;
+                if(i == j){
+                    continue;
+                }else if(shortestPathFromTo[i][j] == i){
+                    betweenVal++;
+                    check = false;
+                }
+
+                Vertex temp = i;
+                Vertex temp2 = shortestPathFromTo[i][j];
+                //checks if edge appears on path, if yes, increment betweenVal counter
+                if(check)
+                while(shortestPathFromTo[temp][j] != graph[j].node_id && temp2 != temp){
+                    Vertex sor1 = toCheck.m_source;
+                    Vertex tar1 = toCheck.m_target;
+                    Vertex sor2 = temp;
+                    Vertex tar2 = temp2;
+
+                    if(sor1 == sor2 && tar1 == tar2){
+                        betweenVal++;
+                        break;
+                    }
+                    temp = temp2;
+                    temp2 = shortestPathFromTo[temp2][j];
+                }
+            }
+    }
+//if new max is reached, clear max list, start new list
+        if(betweenVal > betweenValMax){
+            betweenVal = betweenValMax;
+            maxEdges.clear();
+            maxEdges.push_back(*edge_iter);
+
+        }
+    }
+
+//    std::cout << "max between edges are" << std::endl;
+//    for(int i =0; i < maxEdges.size(); i++){
+//        std::cout << "source: " << maxEdges[i].m_source << std::endl;
+//        std::cout << "target: " << maxEdges[i].m_target << std::endl;
 //
 //
-////            boost::make_iterator_property_map(test.begin(), Vertex)
-//            visitor_type
-//                    <footballGraph, incomingMap, distanceMap, pathCountMap:
-//                    visit;
-//}
+//    }
+
+    return maxEdges;
+}
+void newManAlgo(footballGraph graph, std::vector<std::vector<Vertex>>& paths){
+    while(num_edges(graph) == 0) {
+        std::vector<Edge> maxEdges = calculateBetween(graph, paths);
+        for (int i = 0; i < maxEdges.size(); i++) {
+            graph.remove_edge(maxEdges[i]);
+        }
+
+    }
+}
 
 
 
