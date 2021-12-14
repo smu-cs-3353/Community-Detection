eb2f9c5f9c3b503c569b57d4e7bedcb99447b6bf
milesfried <mafriedman@smu.edu>
2021-12-14 13:23:25 -0600

    build yaml

--- helpful-scripts/dotTopng.py
+++ helpful-scripts/dotTopng.py
@@ -1,7 +1,7 @@
 import pydotplus
 
-graph= pydotplus.graph_from_dot_file('/Users/yeet/Documents/GitHub/21f-pa03-how-bad-can-it-be/output/footbball_output2.dot')
-graph.write_png('/Users/yeet/Documents/GitHub/21f-pa03-how-bad-can-it-be/output/footbball_output2.png')
+graph= pydotplus.graph_from_dot_file('/Users/yeet/Documents/GitHub/21f-pa03-how-bad-can-it-be/output/footbball_output_begin.dot')
+graph.write_png('./output/footbball_output_begin.png')
 
 
 
